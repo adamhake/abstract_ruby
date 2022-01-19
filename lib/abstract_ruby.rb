cb16a15e9888e664b4bad21a2dd1538fe0ddf9eb
adamhake <adam.hake@padillaco.com>
2022-01-19 12:37:43 -0500

    initial gem scaffold

---
+++ lib/abstract_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "abstract_ruby/version"
+
+module AbstractRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/abstract_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AbstractRuby
+  VERSION = "0.1.0"
+end
